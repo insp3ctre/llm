e6d1ad871a3db7257612f868da3cd5c4a46c098a
insp3ctre <adamschwarz101@gmail.com>
2026-05-06 14:39:22 -0400

    stuff

---
+++ functions/run_python_file.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    if not valid_target_dir:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    elif not os.path.isfile(target_dir):
+        return f'Error: "{file_path}" does not exist or is not a regular file'
+    elif not file_path.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file'
+    
+    try:
+        command = ["python", file_path]
+        if args:
+            command.extend(args)
+
+        completed = subprocess.run(command, cwd=working_dir_abs, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=30)
+
+        output = ""
+        if completed.returncode != 0:
+            output += f"Process exited with code {completed.returncode}\n"
+        if completed.stdout and completed.stderr:
+            output += f"No output produced\n"
+        output += f"STDOUT: {completed.stdout}\nSTDERR: {completed.stderr}"
+        return output
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
---
+++ test_run_python_file.py
@@ -0,0 +1,19 @@
+from functions.run_python_file import run_python_file
+
+res = run_python_file("calculator", "main.py")
+print(res)
+
+res = run_python_file("calculator", "main.py", ["3 + 5"])
+print(res)
+
+res = run_python_file("calculator", "tests.py")
+print(res)
+
+res = run_python_file("calculator", "../main.py")
+print(res)
+
+res = run_python_file("calculator", "nonexistent.py")
+print(res)
+
+res = run_python_file("calculator", "lorem.txt")
+print(res)
